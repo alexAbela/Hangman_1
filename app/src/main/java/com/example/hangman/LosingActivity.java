ab8c0251923d655271857583ddf008111f46ecfc
alexAbela <s175124@student.dtu.dk>
2019-11-12 00:08:43 +0100

    CHanged name

--- app/src/main/java/com/example/hangman/LosingActivity.java
+++ app/src/main/java/com/example/hangman/LosingActivity.java
@@ -3,7 +3,10 @@ package com.example.hangman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,11 +15,26 @@ public class LosingActivity extends AppCompatActivity implements View.OnClickLis
 
     private TextView losingWord, losingText;
     private Button restartButton;
+    SharedPreferences preferences;
+    String colour;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_losing);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        colour = preferences.getString("colour","GREEN");
+        if(colour.equalsIgnoreCase("Green")){
+            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+        } else if (colour.equalsIgnoreCase("white")){
+            getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+        } else if (colour.equalsIgnoreCase("red")){
+            getWindow().getDecorView().setBackgroundColor(Color.RED);
+        }
+
+
         Bundle b = getIntent().getExtras();
         String losingWordString = b.getString("losingWord");
 
--- app/src/main/java/com/example/hangman/WinningActivity.java
+++ app/src/main/java/com/example/hangman/WinningActivity.java
@@ -3,7 +3,10 @@ package com.example.hangman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,12 +17,25 @@ public class WinningActivity extends AppCompatActivity implements View.OnClickLi
     private Button restartButton;
     private String winningWordString, winningTextString;
     private int mistakes;
-
+    SharedPreferences preferences;
+    String colour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winning);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        colour = preferences.getString("colour","GREEN");
+        if(colour.equalsIgnoreCase("Green")){
+            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+        } else if (colour.equalsIgnoreCase("white")){
+            getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+        } else if (colour.equalsIgnoreCase("red")){
+            getWindow().getDecorView().setBackgroundColor(Color.RED);
+        }
+
+
         Bundle b = getIntent().getExtras();
         winningWordString = "Ordet var: \n" + b.getString("word");
         mistakes = b.getInt("mistakes");
